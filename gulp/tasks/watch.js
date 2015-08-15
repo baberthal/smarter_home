6534bf09a2bd1297cbd842c55e8b3652075bbcf7
Morgan Lieberthal <morgan@jmorgan.org>
2015-08-15 14:36:54 -0600

    fix up gulp tasks

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -6,6 +6,6 @@ var config = require('../config');
 
 gulp.task('watch', ['sass', 'browserSync'], function(cb) {
     gulp.watch(config.sass.src, function() { gulp.start('sass'); });
-    gulp.watch(config.images.src, function() { gulp.start('sass'); });
-    gulp.watch(config.iconFont.src, function() { gulp.start('sass'); });
+    gulp.watch(config.images.src, function() { gulp.start('images'); });
+    gulp.watch(config.iconFont.src, function() { gulp.start('iconFont'); });
 });
